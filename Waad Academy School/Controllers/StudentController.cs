40da90f5945ea37d4a71fefda639307d8ae090d0
Syedatheruddin <39413237+Syedatheruddin@users.noreply.github.com>
2019-04-24 16:06:35 +0530

    All code here for the first time

---
+++ Waad Academy School/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using ClosedXML.Excel;using System;using System.Collections.Generic;using System.Data;using System.IO;using System.Linq;using System.Web;using System.Web.Mvc;using Waad_Academy_School.Models;namespace Waad_Academy_School.Controllers{
+
+    public class StudentController : Controller    {        private readonly Waad_Academy_SchoolEntities _contextdb;        public StudentController()        {            _contextdb = new Waad_Academy_SchoolEntities();        }
+        // GET: Student
+        public ActionResult Index()        {            return View();        }        public ActionResult WaadAcademy()        {            return View();        }
+        public ActionResult Bloom()
+        {
+            return View();
+        }
+        public ActionResult DarulZikrGirls()
+        {
+            return View();
+        }
+        public ActionResult DarulZikrBoys()
+        {
+            return View();
+        }
+
+        [HttpPost]        public ActionResult StudentSave(Student student)        {            var std = _contextdb.Students.Where(w => w.StudentNo == student.StudentNo.Trim() && w.SchoolId == student.SchoolId).FirstOrDefault();            if (std != null)            {                std.StudentNo = student.StudentNo;                std.SchoolId = student.SchoolId;                std.Latitude = student.Latitude;                std.Longitude = student.Longitude;                std.ModifyOn = DateTime.Now;
+                _contextdb.SaveChanges();            }            else            {                student.CreatedOn = DateTime.Now;                _contextdb.Students.Add(student);                _contextdb.SaveChanges();            }            var response = "Recored Saved";            return Json(response);        }    }}
---
+++ Waad Academy School/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Waad_Academy_School.Startup))]
+namespace Waad_Academy_School
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
